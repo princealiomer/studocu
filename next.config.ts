853bab4c83e3bdd8aecf0a02bb8893c2ed2a9988
unknown <princealiomer@gmail.com>
2025-12-16 14:00:48 +0500

    Fix: Upgrade Node runtime config to 20.x

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
+  serverExternalPackages: ['playwright-core', 'playwright-aws-lambda'],
 };
 
 export default nextConfig;
